5c373ec1ddbb02c6b69ece1272e2c3c39c16864a
nikytania <nikytania0701@gmail.com>
2024-10-03 23:55:38 +0700

    Tugas Jobsheet 6

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -9,16 +9,12 @@ public class Pemilihan2Percobaan118 {
         System.out.print("Masukkan Tahun: ");
         tahun = sc.nextInt();
 
-        if ((tahun % 4) == 0){
-            if((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat: ");
-            else{
-            System.out.println("Bukan Tahun Kabisat !! ");
-            }
-        }else{
-            System.err.println("Bukan Tahun Kabisat");
-        }
+        if ((tahun % 4 == 0 && tahun % 100 != 0) || tahun % 400 == 0){
+            System.out.println( tahun + " adalah tahun kabisat");
 
+        }else{
+            System.out.println( tahun + " bukan tahun kabisat");
+        }
     
             sc.close();
 
