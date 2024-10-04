ab538fb61db9195f8891e5d552c1aafadef0ef0e
nikytania <nikytania0701@gmail.com>
2024-10-04 08:41:11 +0700

    Tugas Jobsheet 6

--- Pemilihan2Percobaan318.java
+++ Pemilihan2Percobaan318.java
@@ -13,7 +13,7 @@ public class Pemilihan2Percobaan318 {
         System.out.print("Masukkan Besarnya Penghasilan : ");
         penghasilan = input18.nextInt();
         
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
@@ -24,7 +24,7 @@ public class Pemilihan2Percobaan318 {
             gajiBersih = (int) (penghasilan - (pajak*penghasilan));
             System.out.print("Penghasil Bersih : " + gajiBersih);
 
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
